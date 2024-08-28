71b4ef4dd349fdaf4007c11fefc1d3d883ce50b2
Aditya <aditya.gup1231@gmail.com>
2024-08-28 18:10:33 +0530

    Initial commit

---
+++ client/src/components/TodoForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import { useDispatch } from "react-redux";
+
+import { addNewTodo } from "../redux/actions";
+
+const TodoForm = () => {
+  const [text, setText] = useState("");
+
+  const dispatch = useDispatch();
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(addNewTodo(text));
+
+    setText("");
+  };
+
+  const onInputChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        placeholder="Write and then Press Enter to Add Todo. . ."
+        className="input"
+        onChange={onInputChange}
+        value={text}
+      />
+    </form>
+  );
+};
+
+export default TodoForm;
