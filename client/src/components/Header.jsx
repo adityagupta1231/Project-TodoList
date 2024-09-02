2b183c0aef61fa2975b5ae8843ec91ef2c9f3eff
adityagupta1231 <122980051+adityagupta1231@users.noreply.github.com>
2024-09-02 23:15:41 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,9 @@
 const Header = () => {
-
-    return (
-        <header>
-            <h1>TodoList</h1>
-        </header>
-    )
-}
+  return (
+    <header>
+      <h1>Things to be done...</h1>
+    </header>
+  );
+};
 
 export default Header;
